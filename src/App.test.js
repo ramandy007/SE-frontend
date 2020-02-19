17de337c31dec74209c1897aedbd2fc739b94a16
ram <ramandy007@gmail.com>
2020-02-19 09:48:36 +0530

    changed course headers in csv

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {
+  render,
+  queryAllByText,
+  findAllByText,
+  getByTitle
+} from "@testing-library/react";
+import App from "./App";
+import { unmountComponentAtNode } from "react-dom";
 
-test('renders learn react link', () => {
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+test("renders login page signup button", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText("Sign up");
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders login page form", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText("Sign up");
   expect(linkElement).toBeInTheDocument();
 });
