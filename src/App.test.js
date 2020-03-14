a275c655ce69233e7badedec49a48ee52e18574b
ram <ramandy007@gmail.com>
2020-03-14 20:54:35 +0530

    changed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,35 +1,35 @@
-import React from "react";
+import React from "react"
 import {
   render,
   queryAllByText,
   findAllByText,
   getByTitle
-} from "@testing-library/react";
-import App from "./App";
-import { unmountComponentAtNode } from "react-dom";
+} from "@testing-library/react"
+import App from "./App"
+import { unmountComponentAtNode } from "react-dom"
 
-let container = null;
+let container = null
 beforeEach(() => {
   // setup a DOM element as a render target
-  container = document.createElement("div");
-  document.body.appendChild(container);
-});
+  container = document.createElement("div")
+  document.body.appendChild(container)
+})
 
 afterEach(() => {
   // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
+  unmountComponentAtNode(container)
+  container.remove()
+  container = null
+})
 
-test("renders login page signup button", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText("Sign up");
-  expect(linkElement).toBeInTheDocument();
-});
+test("renders home page button ", () => {
+  const { getByText } = render(<App />)
+  const linkElement = getByText("Instructions")
+  expect(linkElement).toBeInTheDocument()
+})
 
-test("renders login page form", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText("Sign up");
-  expect(linkElement).toBeInTheDocument();
-});
+test("renders login button", () => {
+  const { getByText } = render(<App />)
+  const linkElement = getByText("login")
+  expect(linkElement).toBeInTheDocument()
+})
